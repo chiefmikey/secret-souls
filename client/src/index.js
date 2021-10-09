4a42ffbaa030b6558373837d08eab4a8f5d0d7fd
chiefmikey <wolfemikl@gmail.com>
2021-10-09 09:22:19 -0600

    Update scene naming convention

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,13 @@
 import K from './functions/init.js';
-import one from './scenes/one.js';
-import two from './scenes/two.js';
+import oneOne from './scenes/1-1.js';
+import oneTwo from './scenes/1-2.js';
+import twoOne from './scenes/2-1.js';
 import win from './scenes/win.js';
 import './functions/controls.js';
 
-K.scene('one', () => one());
-K.scene('two', () => two());
+K.scene('1-1', () => oneOne());
+K.scene('1-2', () => oneTwo());
+K.scene('2-1', () => twoOne());
 K.scene('win', win);
 
-K.start('one');
+K.start('1-1');

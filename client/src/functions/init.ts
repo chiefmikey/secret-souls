1f8ad68862de0b52f08453587c324aa001027efb
Mikl Wolfe <wolfemikl@gmail.com>
2022-01-23 21:20:02 -0700

    Update asset import path

--- client/src/functions/init.ts
+++ client/src/functions/init.ts
@@ -14,21 +14,21 @@ const K = kaboom({
 });
 
 // K.loadRoot('/public/');
-K.loadSprite('ch1', 'assets/sprites/ch1.png');
-K.loadSprite('ch2', 'assets/sprites/ch2.png');
-K.loadSprite('cloudyman', 'assets/sprites/cloudyman.png');
-K.loadSprite('door', 'assets/sprites/door.png');
-K.loadSprite('guy', 'assets/sprites/guy.png');
-K.loadSprite('key', 'assets/sprites/key.png');
-K.loadSprite('wings', 'assets/sprites/key.png');
-K.loadSprite('steel', 'assets/sprites/steel.png');
+K.loadSprite('ch1', 'public/assets/sprites/ch1.png');
+K.loadSprite('ch2', 'public/assets/sprites/ch2.png');
+K.loadSprite('cloudyman', 'public/assets/sprites/cloudyman.png');
+K.loadSprite('door', 'public/assets/sprites/door.png');
+K.loadSprite('guy', 'public/assets/sprites/guy.png');
+K.loadSprite('key', 'public/assets/sprites/key.png');
+K.loadSprite('wings', 'public/assets/sprites/key.png');
+K.loadSprite('steel', 'public/assets/sprites/steel.png');
 
-K.loadSound('coin', 'assets/sounds/coin.mp3');
-K.loadSound('hit', 'assets/sounds/hit.mp3');
-K.loadSound('ayy', 'assets/sounds/ayy.mp3');
-K.loadSound('mmm', 'assets/sounds/mmm.mp3');
-K.loadSound('haha', 'assets/sounds/haha.mp3');
-K.loadSound('aaa', 'assets/sounds/aaa.mp3');
-K.loadSound('priorities', 'assets/sounds/priorities.mp3');
+K.loadSound('coin', 'public/assets/sounds/coin.mp3');
+K.loadSound('hit', 'public/assets/sounds/hit.mp3');
+K.loadSound('ayy', 'public/assets/sounds/ayy.mp3');
+K.loadSound('mmm', 'public/assets/sounds/mmm.mp3');
+K.loadSound('haha', 'public/assets/sounds/haha.mp3');
+K.loadSound('aaa', 'public/assets/sounds/aaa.mp3');
+K.loadSound('priorities', 'public/assets/sounds/priorities.mp3');
 
 export default K;
